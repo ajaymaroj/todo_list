4a929c757bb32e396572898a8a36c38c0bd64d9a
Vijay <vijaykumar212003.com>
2023-01-23 16:28:46 +0530

    comitted

---
+++ to_do_list/todo/middleware/NoDjango_admin_for_end_user_middleware.py
@@ -0,0 +1,12 @@
+
+from django.http import Http404,HttpResponseRedirect
+
+class NoDjangoAdminForEndUserMiddleware:
+  def __init__(self, get_response):
+    self.get_response = get_response
+  def __call__(self, request):
+    if request.path.startswith('/admin/'):
+      if request.user.is_authenticated and not request.user.is_staff:
+        raise Http404
+    response = self.get_response(request)
+    return response 
